d7a6cf35954955a21728b39fd3e8240bc91331b4
zamin77 <46295479+zamin77@users.noreply.github.com>
2022-01-27 18:31:15 +0900

    Add files via upload

---
+++ Assets/Script/GameDirector.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameDirector : MonoBehaviour
+{
+
+    GameObject hpGauge;
+
+    void Start()
+    {
+        this.hpGauge = GameObject.Find("hpGauge");
+    }
+
+    public void DecreaseHP()
+    {
+        this.hpGauge.GetComponent<Image>().fillAmount -= 0.1f;
+    }
+
+
+}
